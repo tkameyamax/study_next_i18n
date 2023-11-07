34882abccf38d5602059e0041d69dbefc4593120
tkameyamax <tkameyamax@gmail.com>
2023-11-07 17:21:26 +0900

    en 辞書をlocalazy経由の文言に修正

--- src/app/i18n/settings.ts
+++ src/app/i18n/settings.ts
@@ -1,5 +1,5 @@
-export const fallbackLng = "en";
-export const languages = [fallbackLng, "jp"];
+export const fallbackLng = "jp";
+export const languages = [fallbackLng, "en"];
 export const defaultNS = "translation";
 export const cookieName = "i18next";
 
